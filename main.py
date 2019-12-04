b797a48e80fefe1486bafabd4944f90c3eaa2ea5
bartosz <tracert1Multi>
2019-12-04 21:47:11 +0100

    s

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import glob
 import re
-
+from reconfigure.parsers import NginxParser
 pattern = r"listen.*\b (443|ssl)\b"
 folder_path = 'conf.d'
 what_file = '*.conf'
@@ -12,12 +12,29 @@ def initial_check_for_ssl():
     for filename in glob.glob(os.path.join(folder_path, what_file)):
         with open(filename, 'r') as f:
 
-            for line in f:
+            f = f.read()
 
-                word = re.findall(pattern, line)
+            config = NginxParser()
+            config = config.parse(content=f)
+            test = config.get_all('server')
 
-                if word:
-                    word.append(filename.strip('conf.d\\')), data.append(word)
+            for a in test:
+                for i in a.get_all('listen'):
+                    if str(i) == 'listen = 443 ssl':
+                        print(a.get('listen'))
+                        print(a.get('server_name'))
+                        print(a.get('ssl_certificate'))
+
+
+
+
+
+            # for line in f:
+            #
+            #     word = re.findall(pattern, line)
+            #
+            #     if word:
+            #         word.append(filename.strip('conf.d\\')), data.append(word)
 
 
 initial_check_for_ssl()
--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-from nginxparser import load
-test=load(open('conf.d/77byte.com.conf'))
-print(test)
+from reconfigure.parsers import NginxParser
+from reconfigure.parsers import JsonParser
+#test=load(open('conf.d/77byte.com.conf'))
+
+json_parser = JsonParser()
+
+with open('conf.d/77byte.com.conf', 'r') as f:
+    f = f.read()
+
+    config = NginxParser()
+    config = config.parse(content=f)
+    test=config.get_all('server')
+
+    for a in test:
+        for i in a.get_all('listen'):
+            if str(i) == 'listen = 443 ssl':
+                print(a.get('server_name'))
+                print(a.get('ssl_certificate'))
+
+
+
+    # print(len(test))
+    # #print(config.children[3])
+    # #print(len(test))
+    # for a in test[3]:
+    #
+    #     print(a)
